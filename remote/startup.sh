419e79dd7acf2d532e13c6c1fcf88a0a6323e8e2
Arjan Molenaar <gaphor@gmail.com>
2021-12-21 12:16:34 +0100

    Can now start a session when remote image is built standalone
    ```bash
    docker build -t capella/remote remote --build-arg BASE_IMAGE=buildpack-deps:bullseye
    docker run -ti --rm -p 3389:3389  -e RMT_PASSWORD=foo8=bar --name capella capella/remote
    ```

--- remote/startup.sh
+++ remote/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 echo -e "tmp_passwd\n$RMT_PASSWORD\n$RMT_PASSWORD" | passwd;
-python /opt/setup_workspace.py
-rm /opt/setup_workspace.py
+python3 /opt/setup_workspace.py
 
-supervisord
+exec supervisord
