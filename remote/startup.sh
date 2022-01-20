96713300fbff5e16db5576f67ebb1cb4621c6f88
MoritzWeber0 <kontakt@moritz-weber.net>
2022-01-20 16:48:17 +0100

    Replace environment variables in /opt/capella/capella.ini
    This will resolve the T4C licence issue and we are able to keep T4C specific configurations out of the remote Dockerfile.
    The T4C licence is still configured in the T4C Dockerfile, but not replaced at this point.

--- remote/startup.sh
+++ remote/startup.sh
@@ -3,4 +3,7 @@ set -e
 echo -e "tmp_passwd\n$RMT_PASSWORD\n$RMT_PASSWORD" | passwd;
 # python3 /opt/setup_workspace.py
 
+# Replace environment variables in capella.ini, e.g. licences
+envsubst < /opt/capella/capella.ini | tee /opt/capella/capella.ini;
+
 exec supervisord
