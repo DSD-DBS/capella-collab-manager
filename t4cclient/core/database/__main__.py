78bd90a50da06967e64927a77b738ff655a39767
MoritzWeber0 <kontakt@moritz-weber.net>
2022-01-24 18:01:19 +0100

    Database: Move migration and init of admin user to module __main__ to prevent recusion

---
+++ t4cclient/core/database/__main__.py
@@ -0,0 +1,41 @@
+import pathlib
+
+from alembic import command
+from alembic.config import Config
+from alembic.migration import MigrationContext
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from t4cclient import config
+from t4cclient.core.database import Base, users
+from t4cclient.schemas.repositories.users import Role
+
+engine = create_engine(config.DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def migrate_db():
+    root_dir = pathlib.Path(__file__).parents[2]
+
+    # Get current revision of Database. If no revision is available, initialize the database.
+    alembic_cfg = Config(root_dir / "alembic.ini")
+    alembic_cfg.set_main_option("script_location", str(root_dir / "alembic"))
+    alembic_cfg.set_main_option("sqlalchemy.url", config.DATABASE_URL)
+    alembic_cfg.attributes["configure_logger"] = False
+
+    engine = create_engine(config.DATABASE_URL)
+    conn = engine.connect()
+
+    context = MigrationContext.configure(conn)
+    current_rev = context.get_current_revision()
+
+    if current_rev:
+        command.upgrade(alembic_cfg, "head")
+    else:
+        Base.metadata.create_all(bind=engine)
+        command.stamp(alembic_cfg, "head")
+        initialize_admin_user()
+
+
+def initialize_admin_user():
+    with SessionLocal() as db:
+        users.create_user(db=db, username=config.INITIAL_ADMIN_USER, role=Role.ADMIN)
