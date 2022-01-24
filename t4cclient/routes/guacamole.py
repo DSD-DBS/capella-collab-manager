5e247caeb90f0c3b7e927338f572a75360434836
Arjan Molenaar <gaphor@gmail.com>
2022-01-24 10:45:05 +0100

    Fix Guacamole login

--- t4cclient/routes/guacamole.py
+++ t4cclient/routes/guacamole.py
@@ -2,7 +2,7 @@ import json
 
 from fastapi import APIRouter, Depends, HTTPException
 from requests import Session
-from t4cclient.config import GUACAMOLE_URI
+from t4cclient.config import GUACAMOLE_URI, USERNAME_CLAIM
 from t4cclient.core.database import get_db, sessions
 from t4cclient.core.oauth.jwt_bearer import JWTBearer
 from t4cclient.extensions import guacamole
@@ -25,7 +25,7 @@ def create_guacamole_token(
     token=Depends(JWTBearer()),
 ):
     session = sessions.get_session_by_id(db, id)
-    if session.owner_name != token[USERNAME]:
+    if session.owner_name != token[USERNAME_CLAIM]:
         raise HTTPException(
             status_code=403,
             detail="The owner of the session does not match with your username.",
