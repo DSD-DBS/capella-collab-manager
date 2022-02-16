674d332560b455c136b46685be3442dcaa45cfd4
MoritzWeber0 <kontakt@moritz-weber.net>
2022-02-16 13:01:39 +0100

    Add entrypoint to readonly container start

--- backend/t4cclient/core/operators/k8s.py
+++ backend/t4cclient/core/operators/k8s.py
@@ -84,7 +84,7 @@ class KubernetesOperator(Operator):
         return self._export_attrs(deployment, service)
 
     def start_readonly_session(
-        self, password: str, git_url: str, git_branch: str
+        self, password: str, git_url: str, git_branch: str, entrypoint: str
     ) -> t.Dict[str, t.Any]:
         id = self._generate_id()
         deployment = self._create_deployment(
@@ -95,6 +95,7 @@ class KubernetesOperator(Operator):
                 "GIT_PASSWORD": config.GIT_PASSWORD,
                 "GIT_REPO_URL": git_url,
                 "GIT_REPO_BRANCH": git_branch,
+                "GIT_ENTRYPOINT": entrypoint,
             },
         )
         self._create_service(id, id)
