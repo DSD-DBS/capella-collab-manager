de29948af3afa935bf09bb921a4fc4325585eb24
MoritzWeber0 <kontakt@moritz-weber.net>
2022-02-18 19:51:18 +0100

    Parse logs of readonly containers to get state

--- backend/t4cclient/core/services/sessions.py
+++ backend/t4cclient/core/services/sessions.py
@@ -1,8 +1,10 @@
+import re
 import typing as t
 
 import t4cclient.extensions.t4c as t4c_manager
 from t4cclient import config
 from t4cclient.core.operators import OPERATOR
+from t4cclient.schemas.sessions import WorkspaceType
 from t4cclient.sql_models.sessions import DatabaseSession
 
 
@@ -12,10 +14,26 @@ def inject_attrs_in_sessions(
     sessions_list = []
     for s in db_sessions:
         session_dict = s.__dict__
-        session_dict["state"] = OPERATOR.get_session_state(session_dict["id"])
+        session_dict["state"] = _determine_session_state(session_dict)
         session_dict["last_seen"] = t4c_manager.fetch_last_seen(session_dict["mac"])
         session_dict["owner"] = session_dict["owner_name"]
 
         sessions_list.append(session_dict)
 
     return sessions_list
+
+
+def _determine_session_state(session: t.Dict[str, t.Any]) -> str:
+    state = OPERATOR.get_session_state(session["id"])
+
+    if session["type"] == WorkspaceType.READONLY:
+        try:
+            if state == "Running":
+                logs = OPERATOR.get_session_logs(session["id"]).splitlines()
+                for line in logs:
+                    res = re.search(r"^---(.*?)---$", line)
+                    if res:
+                        state = res.group(1)
+        except:
+            pass
+    return state
