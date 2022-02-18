f870fb2aa1d800cc902fa20f248a6f8108a71adf
MoritzWeber0 <kontakt@moritz-weber.net>
2022-02-18 18:00:50 +0100

    Improve experience of "Request Session"

--- frontend/src/app/session-creation-progress/session-creation-progress.component.ts
+++ frontend/src/app/session-creation-progress/session-creation-progress.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, Input, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Session } from '../schemes';
+import { BeautifyService } from '../services/beatify/beautify.service';
+import { OwnSessionService } from '../services/own-session/own-session.service';
 
 @Component({
   selector: 'app-session-creation-progress',
   templateUrl: './session-creation-progress.component.html',
-  styleUrls: ['./session-creation-progress.component.css']
+  styleUrls: ['./session-creation-progress.component.css'],
 })
 export class SessionCreationProgressComponent implements OnInit {
+  @Input()
+  sessionType = '';
 
-  constructor() { }
+  @Input()
+  session: Session | undefined = undefined;
 
-  ngOnInit(): void {
+  refreshSessionsSubscription: Subscription;
+
+  containerReady = false;
+
+  constructor(
+    private ownSessionService: OwnSessionService,
+    public beautifyService: BeautifyService
+  ) {
+    this.refreshSessionsSubscription = timer(0, 2000)
+      .pipe(
+        map(() => {
+          if (this.session !== undefined) {
+            this.ownSessionService
+              .refreshSessions()
+              .subscribe((res: Array<Session>) => {
+                const session = res.find(
+                  (session: Session) => session.id === this.session?.id
+                );
+
+                if (session !== undefined) {
+                  this.session = JSON.parse(JSON.stringify(session));
+                }
+
+                if (this.session?.state === 'Running') {
+                  this.containerReady = true;
+                  this.refreshSessionsSubscription.unsubscribe();
+                }
+              });
+          }
+        })
+      )
+      .subscribe();
   }
 
+  ngOnInit(): void {}
 }
