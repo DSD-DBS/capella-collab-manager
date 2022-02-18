b2e03855c490e13056840daec10ca420d868b20a
MoritzWeber0 <kontakt@moritz-weber.net>
2022-02-18 09:53:57 +0100

    Initial sketch of session state

---
+++ frontend/src/app/session-creation-progress/session-creation-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-session-creation-progress',
+  templateUrl: './session-creation-progress.component.html',
+  styleUrls: ['./session-creation-progress.component.css']
+})
+export class SessionCreationProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
