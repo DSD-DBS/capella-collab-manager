b550f84415da51c1f4bbd7768baae452cb21f496
MoritzWeber0 <kontakt@moritz-weber.net>
2022-03-03 13:04:14 +0100

    Replace RepositoryProject service with T4CRepoService

---
+++ frontend/src/app/services/modelsources/t4c-repos/t4c-repo.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { T4CRepoService } from './t4c-repo.service';
+
+describe('T4CRepoService', () => {
+  let service: T4CRepoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(T4CRepoService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- frontend/src/app/services/modelsources/t4c-repos/t4c-repo.service.ts
+++ frontend/src/app/services/modelsources/t4c-repos/t4c-repo.service.ts
@@ -1,27 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RepositoryProjectService {
+export class T4CRepoService {
   constructor(private http: HttpClient) {}
+  repositories: Array<T4CRepository> = [];
 
-  getRepositoryProjects(
-    repository: string
-  ): Observable<Array<RepositoryProject>> {
-    return this.http.get<Array<RepositoryProject>>(
-      `${environment.backend_url}/repositories/${repository}/projects`
-    );
+  getRepositoryProjects(repository: string): Observable<Array<T4CRepository>> {
+    return this.http
+      .get<Array<T4CRepository>>(
+        `${environment.backend_url}/repositories/${repository}/projects`
+      )
+      .pipe(
+        tap((res: Array<T4CRepository>) => {
+          this.repositories = res;
+        })
+      );
   }
 
   createRepositoryProject(
     repository: string,
     project_name: string
-  ): Observable<RepositoryProject> {
-    return this.http.post<RepositoryProject>(
+  ): Observable<T4CRepository> {
+    return this.http.post<T4CRepository>(
       `${environment.backend_url}/repositories/${repository}/projects`,
       { name: project_name }
     );
@@ -30,14 +36,14 @@ export class RepositoryProjectService {
   deleteRepositoryProject(
     repository: string,
     project_id: number
-  ): Observable<RepositoryProject> {
-    return this.http.delete<RepositoryProject>(
+  ): Observable<T4CRepository> {
+    return this.http.delete<T4CRepository>(
       `${environment.backend_url}/repositories/${repository}/projects/${project_id}`
     );
   }
 }
 
-export interface RepositoryProject {
+export interface T4CRepository {
   id: number;
   name: string;
   repository_name: string;
