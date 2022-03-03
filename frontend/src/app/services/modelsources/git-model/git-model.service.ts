a988b30cfa3e5684b67da3ee2b20b4bd1fd703a8
MoritzWeber0 <kontakt@moritz-weber.net>
2022-03-03 13:05:54 +0100

    Restructuring

--- frontend/src/app/services/modelsources/git-model/git-model.service.ts
+++ frontend/src/app/services/modelsources/git-model/git-model.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,16 +9,23 @@ import { environment } from 'src/environments/environment';
 })
 export class GitModelService {
   constructor(private http: HttpClient) {}
+  models: Array<GitModel> = [];
 
   getGitRepositoriesForRepository(
     repository_name: string
   ): Observable<Array<GitModel>> {
-    return this.http.get<Array<GitModel>>(
-      environment.backend_url +
-        '/repositories/' +
-        repository_name +
-        '/git-models/'
-    );
+    return this.http
+      .get<Array<GitModel>>(
+        environment.backend_url +
+          '/repositories/' +
+          repository_name +
+          '/git-models/'
+      )
+      .pipe(
+        tap((res: Array<GitModel>) => {
+          this.models = res;
+        })
+      );
   }
 
   unassignGitRepositoriesFromRepository(
