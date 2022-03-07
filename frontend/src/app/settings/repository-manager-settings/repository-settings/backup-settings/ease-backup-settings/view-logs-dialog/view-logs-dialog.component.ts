71c2a8ea548cab54629bfaf97c28727aead45a4d
MoritzWeber0 <kontakt@moritz-weber.net>
2022-03-07 18:13:14 +0100

    Refresh logs automatically

--- frontend/src/app/settings/repository-manager-settings/repository-settings/backup-settings/ease-backup-settings/view-logs-dialog/view-logs-dialog.component.ts
+++ frontend/src/app/settings/repository-manager-settings/repository-settings/backup-settings/ease-backup-settings/view-logs-dialog/view-logs-dialog.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Subscription, timer } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { EASEBackupService } from 'src/app/services/backups/ease/easebackup.service';
 
 @Component({
@@ -7,11 +9,21 @@ import { EASEBackupService } from 'src/app/services/backups/ease/easebackup.serv
   templateUrl: './view-logs-dialog.component.html',
   styleUrls: ['./view-logs-dialog.component.css'],
 })
-export class ViewLogsDialogComponent implements OnInit {
+export class ViewLogsDialogComponent implements OnInit, OnDestroy {
+  refreshLogsSubscription: Subscription;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: ViewLogsData,
     private easeBackupService: EASEBackupService
-  ) {}
+  ) {
+    this.refreshLogsSubscription = timer(0, 5000)
+      .pipe(
+        tap(() => {
+          this.refreshLogs();
+        })
+      )
+      .subscribe();
+  }
 
   loading = true;
   logs = '';
@@ -20,7 +32,12 @@ export class ViewLogsDialogComponent implements OnInit {
     this.refreshLogs();
   }
 
+  ngOnDestroy(): void {
+    this.refreshLogsSubscription.unsubscribe();
+  }
+
   refreshLogs(): void {
+    this.loading = true;
     this.easeBackupService
       .getLogs(this.data.project, this.data.backup_id, this.data.job_id)
       .subscribe(
