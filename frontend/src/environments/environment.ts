ec12289ef05ea45a43fcd5e647467253f3cc0050
MoritzWeber0 <kontakt@moritz-weber.net>
2022-03-25 18:24:56 +0100

    Add Authentication type as option

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,4 +3,5 @@ export const environment = {
   backend_url: '/api/v1',
   privacy: 'https://example.com/privacy',
   imprint: 'https://example.com/imprint',
+  authentication: 'AUTHENTICATION_TYPE',
 };
