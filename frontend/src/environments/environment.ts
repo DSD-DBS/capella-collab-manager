61831a1b49de1d39c89f197ee7a02c6b05c37364
MoritzWeber0 <kontakt@moritz-weber.net>
2022-04-25 10:33:52 +0200

    Customize platform provider

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   backend_url: '/api/v1',
   privacy: 'https://example.com/privacy',
   imprint: 'https://example.com/imprint',
+  provider: 'PROVIDER',
   authentication: 'AUTHENTICATION_TYPE',
   usernameAttribute: 'attribute',
 };
