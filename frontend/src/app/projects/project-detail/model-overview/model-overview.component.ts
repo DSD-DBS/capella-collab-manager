bb225194572474e1d37048974aee2f9cb29229fd
MoritzWeber0 <kontakt@moritz-weber.net>
2022-05-23 11:50:51 +0200

    Add buttons to ModelOverview

--- frontend/src/app/projects/project-detail/model-overview/model-overview.component.ts
+++ frontend/src/app/projects/project-detail/model-overview/model-overview.component.ts
@@ -1,18 +1,18 @@
 // Copyright DB Netz AG and the capella-collab-manager contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-model-overview',
   templateUrl: './model-overview.component.html',
-  styleUrls: ['./model-overview.component.css']
+  styleUrls: ['./model-overview.component.css'],
 })
 export class ModelOverviewComponent implements OnInit {
+  @Input()
+  project = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
