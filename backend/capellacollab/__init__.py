6b8f7df757cecab13628d0023314c21e0aae4729
MoritzWeber0 <kontakt@moritz-weber.net>
2022-05-23 12:27:01 +0200

    Introduce versioneer
    Resolves #97

--- backend/capellacollab/__init__.py
+++ backend/capellacollab/__init__.py
@@ -1,3 +1,6 @@
 # Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
 
+
+from . import _version
+__version__ = _version.get_versions()['version']
