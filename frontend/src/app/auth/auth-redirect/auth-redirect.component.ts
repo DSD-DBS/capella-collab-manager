ff2f416611f903f0ec0f8aa19377d8e0c1b5da67
Timothé Bailly-Barthez <timothe.bailly-barthez@deutschebahn.com>
2022-06-08 11:23:36 +0200

    handling oauth errors
    When the oauth callback has an error parameter, the user is redirected to login page and is shown an error card containing informations about the error according to OAuth2 specification

--- frontend/src/app/auth/auth-redirect/auth-redirect.component.ts
+++ frontend/src/app/auth/auth-redirect/auth-redirect.component.ts
@@ -27,6 +27,16 @@ export class AuthRedirectComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
+      if (params['error']) {
+        const test = Object.keys(params).map(key => {
+          key
+        })
+        const redirect_url = '/auth?' + Object.keys(params).map(key => 
+          ['error', 'error_description', 'error_uri'].includes(key) ? [key, params[key]].join('=') : ""
+        ).join('&')
+        this.router.navigateByUrl(redirect_url)
+        return
+      }
       this.authService
         .getAccessToken(params['code'], params['state'])
         .subscribe((res) => {
