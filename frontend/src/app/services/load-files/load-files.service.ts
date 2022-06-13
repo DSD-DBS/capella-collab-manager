d68233d0c078469a94cb1aab1768ecf8d287ff48
MoritzWeber0 <kontakt@moritz-weber.net>
2022-06-13 09:22:24 +0200

    Rework upload of files
    - typing was added to backend routes
    - json is used for communication between frontend and backend
    - add path to FileTree elements, replaces level
    - Use '/files' route (to make it REST compliant)
    - remove unnecessary exception handling (fastAPI automatically handles 500 errors)
    - add progress bar during loading of the tree
    - add files to tree when they are added by the user
    - rename css classes

--- frontend/src/app/services/load-files/load-files.service.ts
+++ frontend/src/app/services/load-files/load-files.service.ts
@@ -1,34 +1,35 @@
+// Copyright DB Netz AG and the capella-collab-manager contributors
+// SPDX-License-Identifier: Apache-2.0
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoadFilesService {
-
   BACKEND_URL_PREFIX = environment.backend_url + '/sessions/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   upload(id: string, files: FormData): Observable<any> {
-    return this.http.post<any>(
-      this.BACKEND_URL_PREFIX + id,
-      files,
-      {
-        reportProgress: true,
-        observe: 'events'
-      }
-    );
+    return this.http.post<any>(this.BACKEND_URL_PREFIX + id + '/files', files, {
+      reportProgress: true,
+      observe: 'events',
+    });
   }
 
-  getCurrentFiles(id: string): Observable<string> {
-    return this.http.get<string>(this.BACKEND_URL_PREFIX + id);
+  getCurrentFiles(id: string): Observable<FileTree> {
+    return this.http.get<FileTree>(this.BACKEND_URL_PREFIX + id + '/files');
   }
+}
 
-  download(file: string) {
-
-  }
-
+export interface FileTree {
+  path: string;
+  name: string;
+  type: 'file' | 'directory';
+  children: FileTree[] | null;
+  newFile: boolean;
 }
