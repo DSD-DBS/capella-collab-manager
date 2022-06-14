995f0d5067011fe95ed65333dc176e14a2de5b85
henrik997 <h.strangalies@web.de>
2022-06-14 11:54:05 +0200

    Check that user belongs to session

--- backend/t4cclient/core/authentication/database/__init__.py
+++ backend/t4cclient/core/authentication/database/__init__.py
@@ -6,6 +6,7 @@ from fastapi import Depends, HTTPException
 from t4cclient.core.authentication.helper import get_username
 from t4cclient.core.authentication.jwt_bearer import JWTBearer
 from t4cclient.core.database import get_db, repository_users
+from t4cclient.core.database.sessions import get_sessions_for_user, get_session_by_id
 from t4cclient.core.database.users import get_user
 from t4cclient.schemas.repositories import RepositoryUserPermission, RepositoryUserRole
 from t4cclient.schemas.repositories.users import Role
@@ -102,4 +103,18 @@ def check_username_not_in_repository(
         raise HTTPException(
             status_code=409,
             detail="The user already exists for this repository.",
+        )
+
+
+def check_session_belongs_to_user(
+    username: str,
+    id: str,
+    db: sqlalchemy.orm.session.Session,
+):
+    sessions = get_sessions_for_user(db, username)
+    session = get_session_by_id(db, id)
+    if not session in sessions:
+        raise HTTPException(
+            status_code=403,
+            detail="You are not allowed to upload or get files in this session.",
         )
