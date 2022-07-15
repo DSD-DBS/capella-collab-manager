6ffeb6a813d09ed48d14875fbb4dd7353597db90
MoritzWeber0 <kontakt@moritz-weber.net>
2022-07-15 17:46:10 +0200

    feat:  log mock messages for debugging

--- mocks/t4c-server/mock/users.py
+++ mocks/t4c-server/mock/users.py
@@ -1,11 +1,13 @@
 # Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
 
+import logging
+
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-
 router = APIRouter()
+logger = logging.getLogger()
 
 
 class PostUserRequest(BaseModel):
@@ -16,14 +18,17 @@ class PostUserRequest(BaseModel):
 
 @router.post("/")
 def add_user(body: PostUserRequest, repositoryName: str):
+    logger.info("Added user with body %s.", body)
     return body
 
 
 @router.delete("/{userName}")
-def remove_user():
+def remove_user(userName: str):
+    logger.info("Removed user %s.", userName)
     return {}
 
 
 @router.put("/{userName}")
-def update_user():
+def update_user(userName: str):
+    logger.info("Updated user %s.")
     return {}
