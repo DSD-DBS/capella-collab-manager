57d7f75124a4c27c9e12e03bba0841535385ae15
MoritzWeber0 <kontakt@moritz-weber.net>
2022-08-02 14:08:51 +0200

    feat: add custom runtime no_proxy support

--- backend/startup.sh
+++ backend/startup.sh
@@ -2,5 +2,5 @@
 set -e
 
 # When running inside the cluster, the k8s service host should not use the proxy
-export no_proxy=$no_proxy,$KUBERNETES_SERVICE_HOST
+export no_proxy=$no_proxy,$KUBERNETES_SERVICE_HOST,$no_proxy_additional
 uvicorn t4cclient.__main__:app --host 0.0.0.0
