4ef71d85c6832ebde6ae0b711e1345689d166309
henrik997 <h.strangalies@web.de>
2022-08-18 16:13:03 +0200

    feat: add overview of staged projects
    If a staged project is selected, open delete project dialog

--- frontend/src/app/settings/requests/staged-projects-overview/staged-projects-overview.component.ts
+++ frontend/src/app/settings/requests/staged-projects-overview/staged-projects-overview.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { DeleteProjectDialogComponent } from 'src/app/projects/delete-project/delete-project-dialog/delete-project-dialog.component';
+import {
+  Project,
+  ProjectService,
+} from 'src/app/services/project/project.service';
 
 @Component({
   selector: 'app-staged-projects-overview',
@@ -6,7 +13,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staged-projects-overview.component.css'],
 })
 export class StagedProjectsOverviewComponent implements OnInit {
-  constructor(private projectService: ProjectService) {}
+  stagedProjects: Array<Project> = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private projectService: ProjectService,
+    private dialog: MatDialog,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.projectService.list().subscribe((projects: Array<Project>) => {
+      this.stagedProjects = projects.filter((project) => project.staged_by);
+    });
+  }
+
+  openDeleteDialog(project_name: string) {
+    const deleteProjectDialog = this.dialog.open(DeleteProjectDialogComponent, {
+      data: { project_name: project_name, overview: true },
+    });
+    deleteProjectDialog.afterClosed().subscribe((response) => {
+      if (response) {
+        this.projectService.deleteProject(project_name).subscribe({
+          complete: () => this.router.navigateByUrl('/settings/requests'),
+        });
+      }
+    });
+  }
 }
