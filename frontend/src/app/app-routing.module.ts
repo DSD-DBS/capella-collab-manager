cdf18980f2b9589f569e18f156bfbe84778b7564
MoritzWeber0 <kontakt@moritz-weber.net>
2022-09-27 14:00:23 +0200

    chore: Remove unused imports

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -29,11 +29,6 @@ import { T4CSettingsComponent } from './settings/modelsources/t4c-settings/t4c-s
 import { RequestsComponent } from './settings/requests/requests.component';
 import { SettingsComponent } from './settings/settings.component';
 import { HomeComponent } from './workspaces/home.component';
-import { CreateModelBaseComponent } from './models/create-model-base/create-model-base.component';
-import { CreateCoworkingMethodComponent } from './models/create-coworking-method/create-coworking-method.component';
-import { ChooseSourceComponent } from './models/choose-source/choose-source.component';
-import { InitModelComponent } from './models/init-model/init-model.component';
-import { ChooseInitComponent } from './models/choose-init/choose-init.component';
 import { ProjectWrapperComponent } from './projects/project-wrapper/project-wrapper.component';
 import { ModelWrapperComponent } from './models/model-wrapper/model-wrapper.component';
 import { CreateModelComponent } from './models/create-model/create-model.component';
