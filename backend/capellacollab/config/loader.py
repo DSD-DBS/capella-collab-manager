1c4ce2d5546f510a89ed38c4cee63a32437f16c3
Arjan Molenaar <gaphor@gmail.com>
2022-09-29 12:30:51 +0200

    fix: Provide a warning when loading a fallback config

--- backend/capellacollab/config/loader.py
+++ backend/capellacollab/config/loader.py
@@ -16,6 +16,9 @@ locations: list[pathlib.Path] = [
     pathlib.Path(appdirs.user_config_dir("capellacollab", "db"))
     / "config.yaml",
     pathlib.Path("/etc/capellacollab/config.yaml"),
+]
+
+fallback_locations: list[pathlib.Path] = [
     pathlib.Path(__file__).parents[2] / "config" / "config_template.yaml",
 ]
 
@@ -24,11 +27,18 @@ def load_yaml() -> None | dict:
     log.debug("Searching for configuration files...")
     for l in locations:
         if l.exists():
-            log.info("Found configuration file at location %s", str(l))
+            log.info("Loading configuration file at location %s", str(l))
             return yaml.safe_load(l.open())
         else:
             log.debug(
                 "Didn't find a configuration file at location %s", str(l)
             )
+
+    for l in fallback_locations:
+        if l.exists():
+            log.warning(
+                "Loading fallback configuration file at location %s", str(l)
+            )
+            return yaml.safe_load(l.open())
 
     return None
