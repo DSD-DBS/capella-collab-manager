af2e0f64f9c8554e2a0a111ecdf513e56dccc1eb
MoritzWeber <kontakt@moritz-weber.net>
2022-10-05 13:54:26 +0200

    chore: Remove git-utils
    There were two workstreams which implemented the git workflow.
    This PR removes one workstream.
    
    Resolves #220

--- backend/capellacollab/routes/__init__.py
+++ backend/capellacollab/routes/__init__.py
@@ -17,7 +17,6 @@ from capellacollab.config import config
 from capellacollab.projects import routes as projects
 from capellacollab.settings.modelsources.git import routes as git_settings
 from capellacollab.tools import routes as tools
-from capellacollab.utils import git_utils
 
 log = logging.getLogger(__name__)
 
@@ -40,7 +39,6 @@ router.include_router(
 router.include_router(
     configuration.router, prefix="/configurations", tags=["Notices"]
 )
-router.include_router(git_utils.router, prefix="/git-utils")
 
 # Load authentication routes
 try:
