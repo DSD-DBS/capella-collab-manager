224c58a24bcf49e36d966ad2f339f3e1fa61f8fc
dominik003 <info@dominik-lammers.de>
2022-10-05 08:31:56 +0200

    feat: Add git authentication support
    This is required to be able to authenticate with Git.
    For this purpose, Git uses the git_askpass.py file,
     which retrieves the environment variables for the
     username and password and uses them for the authentication.

---
+++ backend/git_askpass.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+
+if __name__ == "__main__":
+    print(os.environ["GIT_" + sys.argv[1].split()[0].upper()])
+    raise SystemExit(0)
