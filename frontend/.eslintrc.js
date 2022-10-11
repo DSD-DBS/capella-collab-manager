aa26d08478979efacec9acbcd541019643277bd2
MoritzWeber <kontakt@moritz-weber.net>
2022-10-11 20:17:19 +0200

    feat: Add ESLint as linter

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["standard-with-typescript", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "@typescript-eslint/prefer-readonly": "off",
+  },
+};
