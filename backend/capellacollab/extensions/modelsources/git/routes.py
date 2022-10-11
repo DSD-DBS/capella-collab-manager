43a2b19520eb017d9ce77ff8656e4d821f94cbce
dominik003 <info@dominik-lammers.de>
2022-10-11 14:32:32 +0200

    refactor: Remove unused routes

--- backend/capellacollab/extensions/modelsources/git/routes.py
+++ backend/capellacollab/extensions/modelsources/git/routes.py
@@ -12,9 +12,6 @@ from requests import Session
 
 import capellacollab.projects.crud as projects_crud
 from capellacollab.core.authentication.database import verify_project_role
-from capellacollab.core.authentication.database.git_models import (
-    verify_gitmodel_permission,
-)
 from capellacollab.core.authentication.jwt_bearer import JWTBearer
 from capellacollab.core.authentication.responses import (
     AUTHENTICATION_RESPONSES,
@@ -32,6 +29,7 @@ from capellacollab.extensions.modelsources.git.models import (
     PatchRepositoryGitModel,
     PostGitModel,
     RepositoryGitInnerModel,
+    ResponseGitModel,
     ResponseGitSource,
 )
 
@@ -40,27 +38,6 @@ from .core import get_remote_refs, ls_remote
 
 router = APIRouter()
 log = logging.getLogger(__name__)
-
-
-@router.get(
-    "/",
-    response_model=t.List[GetRepositoryGitModel],
-    responses=AUTHENTICATION_RESPONSES,
-)
-def get_models_for_repository(
-    project: str, db: Session = Depends(get_db), token=Depends(JWTBearer())
-):
-    verify_project_role(project, token=token, db=db)
-    db_models = git.crud.get_models_of_repository(db, project)
-    return_models: t.List[GetRepositoryGitModel] = []
-    for db_model in db_models:
-        return_models.append(
-            GetRepositoryGitModel(
-                **db_model.__dict__,
-                model=RepositoryGitInnerModel(**db_model.__dict__),
-            )
-        )
-    return return_models
 
 
 @router.post("/create/{model_slug}", response_model=ResponseGitSource)
@@ -75,71 +52,7 @@ def create_source(
     project_instance = projects_crud.get_project(db, project)
     verify_project_role(project_instance.name, token, db)
     new_source = crud.create(db, project_instance.slug, model_slug, source)
-    return ResponseGitSource.from_db_git_source(new_source)
-
-
-@router.post(
-    "/",
-    response_model=GetRepositoryGitModel,
-    responses=AUTHENTICATION_RESPONSES,
-)
-def assign_model_to_repository(
-    project: str,
-    body: PostGitModel,
-    db: Session = Depends(get_db),
-    token=Depends(JWTBearer()),
-):
-    verify_project_role(
-        project, allowed_roles=["manager", "administrator"], token=token, db=db
-    )
-    body.model.path = base64.b64decode(body.model.path).decode("utf-8")
-    db_model = git.crud.add_model_to_repository(db, project, body)
-    return GetRepositoryGitModel(
-        **db_model.__dict__,
-        model=RepositoryGitInnerModel(**db_model.__dict__),
-    )
-
-
-@router.delete(
-    "/{model_id}",
-    status_code=204,
-    responses=AUTHENTICATION_RESPONSES,
-)
-def unassign_model_from_repository(
-    project: str,
-    model_id: int,
-    db: Session = Depends(get_db),
-    token=Depends(JWTBearer()),
-):
-    verify_project_role(
-        project, allowed_roles=["manager", "administrator"], token=token, db=db
-    )
-    verify_gitmodel_permission(project, model_id, db)
-    git.crud.delete_model_from_repository(db, project, model_id)
-    return None
-
-
-@router.patch(
-    "/{model_id}",
-    response_model=GetRepositoryGitModel,
-    responses=AUTHENTICATION_RESPONSES,
-)
-def patch_model(
-    project: str,
-    body: PatchRepositoryGitModel,
-    model_id: int,
-    db: Session = Depends(get_db),
-    token=Depends(JWTBearer()),
-):
-    verify_project_role(project, token=token, db=db)
-    verify_gitmodel_permission(project, model_id, db)
-    if body.primary is not None:
-        db_model = git.crud.make_model_primary(db, project, model_id)
-        return GetRepositoryGitModel(
-            **db_model.__dict__,
-            model=RepositoryGitInnerModel(**db_model.__dict__),
-        )
-    return None
+    return ResponseGitSource.from_orm(new_source)
 
 
 @router.get(
