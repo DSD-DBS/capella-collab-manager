eb6e165ec379e39aed150ba8fc8725e3d1fb383b
dominik003 <info@dominik-lammers.de>
2022-10-12 10:19:03 +0200

    refactor: Remove legacy license header

--- frontend/src/app/services/settings/git-settings.service.spec.ts
+++ frontend/src/app/services/settings/git-settings.service.spec.ts
@@ -3,9 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// Copyright DB Netz AG and the capella-collab-manager contributors
-// SPDX-License-Identifier: Apache-2.0
-
 import { TestBed } from '@angular/core/testing';
 
 import { GitSettingsService } from './git-settings.service';
