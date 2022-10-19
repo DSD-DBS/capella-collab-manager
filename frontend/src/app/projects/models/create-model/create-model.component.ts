800b5d9911ce98770a94b01584a288150b151365
dominik003 <info@dominik-lammers.de>
2022-10-19 16:43:09 +0200

    fix: Adjust parameters

--- frontend/src/app/projects/models/create-model/create-model.component.ts
+++ frontend/src/app/projects/models/create-model/create-model.component.ts
@@ -67,8 +67,8 @@ export class CreateModelComponent implements OnDestroy {
     this.stepper.next();
   }
 
-  afterSourceCreated(options: { created: boolean }): void {
-    if (options.created) {
+  afterSourceCreated(created: boolean): void {
+    if (created) {
       this.stepper.steps.get(2)!.completed = true;
       this.stepper.next();
       this.stepper.steps.get(1)!.editable = false;
