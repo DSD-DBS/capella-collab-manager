ea18f0cc7485d8381106f11a0f85e3e5396657db
dominik003 <info@dominik-lammers.de>
2022-10-20 17:51:21 +0200

    refactor: Remove legacy resources

--- backend/capellacollab/extensions/__init__.py
+++ backend/capellacollab/extensions/__init__.py
@@ -1,20 +1,2 @@
 # SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
 # SPDX-License-Identifier: Apache-2.0
-
-
-from importlib import metadata
-
-
-def load_modelsource(name: str):
-    try:
-        ep = next(
-            i
-            for i in metadata.entry_points()[
-                "capellacollab.extensions.modelsources"
-            ]
-            if i.name == name
-        )
-    except StopIteration:
-        raise ValueError(f"Unknown modelsource {name}")
-
-    return ep.load()
