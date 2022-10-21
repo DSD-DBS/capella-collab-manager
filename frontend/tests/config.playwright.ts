4736f822cfc6d57a6000851b130b3949a5408a53
dominik003 <info@dominik-lammers.de>
2022-10-21 14:00:59 +0200

    feat: Add custom playwright config

---
+++ frontend/tests/config.playwright.ts
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const config = {
+  backend_url: 'http://localhost:8000/api/v1',
+  frontend_url: 'http://localhost:4200',
+};
