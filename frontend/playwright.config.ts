a22db2b5767e6d73753376104a11d5fb5fe5a2cb
dominik003 <info@dominik-lammers.de>
2022-10-21 14:04:46 +0200

    chore: Adjust configuration

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -91,7 +91,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'ng serve',
     port: 4200,
   },
 };
