18956cdc39812f1747d3d0325472932a33c19d96
MoritzWeber <kontakt@moritz-weber.net>
2022-10-24 08:33:17 +0200

    refactor: fix typing annotations

--- backend/capellacollab/tools/routes.py
+++ backend/capellacollab/tools/routes.py
@@ -33,7 +33,7 @@ from . import crud
 router = APIRouter()
 
 
-def get_existing_tool(tool_id: str, db: Session = Depends(get_db)) -> Tool:
+def get_existing_tool(tool_id: int, db: Session = Depends(get_db)) -> Tool:
     try:
         return crud.get_tool_by_id(id_=tool_id, db=db)
     except sqlalchemy.exc.NoResultFound:
@@ -47,7 +47,7 @@ def get_existing_tool(tool_id: str, db: Session = Depends(get_db)) -> Tool:
 
 
 def get_exisiting_tool_version(
-    tool_id: str, version_id, db: Session = Depends(get_db)
+    tool_id: int, version_id: int, db: Session = Depends(get_db)
 ) -> Version:
     try:
         return crud.get_version_for_tool(tool_id, version_id, db)
@@ -62,7 +62,7 @@ def get_exisiting_tool_version(
 
 
 def get_exisiting_tool_type(
-    tool_id: str, type_id, db: Session = Depends(get_db)
+    tool_id: int, type_id: int, db: Session = Depends(get_db)
 ) -> Type:
     try:
         return crud.get_type_for_tool(tool_id, type_id, db)
