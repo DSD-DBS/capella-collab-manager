debc0f76969a4cc83b52810e8866b20822e4de07
MoritzWeber <kontakt@moritz-weber.net>
2022-10-27 13:18:45 +0200

    refactor: Remove sql_models module

---
+++ backend/capellacollab/core/database/models.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
+# SPDX-License-Identifier: Apache-2.0
+
+# These import statements of the models are required and should not be removed! (SQLAlchemy will not load the models otherwise)
+import capellacollab.notices.models
+import capellacollab.projects.models
+import capellacollab.sessions.models
+import capellacollab.tools.models
+import capellacollab.users.models
--- backend/capellacollab/sql_models/extensions.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
-# SPDX-License-Identifier: Apache-2.0
-
-
-import importlib
-import logging
-from importlib import metadata
-
-log = logging.getLogger(__name__)
