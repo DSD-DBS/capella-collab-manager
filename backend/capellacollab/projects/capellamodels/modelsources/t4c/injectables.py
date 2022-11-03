91d24c3242b36ed077dac637b63ec4195ef18c77
MoritzWeber <kontakt@moritz-weber.net>
2022-11-03 08:20:50 +0100

    fix!: Check if T4C model is part of the Capella model

--- backend/capellacollab/projects/capellamodels/modelsources/t4c/injectables.py
+++ backend/capellacollab/projects/capellamodels/modelsources/t4c/injectables.py
@@ -6,6 +6,10 @@ from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 
 from capellacollab.core.database import get_db
+from capellacollab.projects.capellamodels.injectables import (
+    get_existing_capella_model,
+)
+from capellacollab.projects.capellamodels.models import DatabaseCapellaModel
 from capellacollab.projects.capellamodels.modelsources.t4c import crud
 from capellacollab.projects.capellamodels.modelsources.t4c.models import (
     DatabaseT4CModel,
@@ -13,14 +17,19 @@ from capellacollab.projects.capellamodels.modelsources.t4c.models import (
 
 
 def get_existing_t4c_model(
-    t4c_model_id: int, db: Session = Depends(get_db)
+    t4c_model_id: int,
+    capella_model: DatabaseCapellaModel = Depends(get_existing_capella_model),
+    db: Session = Depends(get_db),
 ) -> DatabaseT4CModel:
     try:
-        return crud.get_t4c_model_by_id(db, t4c_model_id)
+        t4c_model = crud.get_t4c_model_by_id(db, t4c_model_id)
+        if t4c_model.model_id == capella_model.id:
+            return t4c_model
+
     except NoResultFound:
         raise HTTPException(
             404,
             {
-                "reason": f"The model with the id {t4c_model_id} does not exist."
+                "reason": f"The t4c model ({t4c_model_id}) does not exists on the capella model with the ID {capella_model}."
             },
         )
