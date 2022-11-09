c4c348d341bacb565e4ade25d4c5246b00952650
MoritzWeber <kontakt@moritz-weber.net>
2022-11-09 17:16:00 +0100

    feat: Copy t4c session token to clipboard and handle non existence

--- frontend/src/app/sessions/session-created/guacamole/guacamole.component.ts
+++ frontend/src/app/sessions/session-created/guacamole/guacamole.component.ts
@@ -6,6 +6,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { LocalStorageService } from 'src/app/general/auth/local-storage/local-storage.service';
+import { ToastService } from 'src/app/helpers/toast/toast.service';
 import { Session } from 'src/app/schemes';
 import { GuacamoleService } from 'src/app/services/guacamole/guacamole.service';
 import { UserService } from 'src/app/services/user/user.service';
@@ -23,7 +24,8 @@ export class GuacamoleComponent {
     private localStorageService: LocalStorageService,
     private guacamoleService: GuacamoleService,
     @Inject(MAT_DIALOG_DATA) public session: Session,
-    public dialogRef: MatDialogRef<GuacamoleComponent>
+    public dialogRef: MatDialogRef<GuacamoleComponent>,
+    private toastService: ToastService
   ) {}
 
   redirectToGuacamole(): void {
@@ -33,5 +35,12 @@ export class GuacamoleComponent {
         this.localStorageService.setValue('GUAC_AUTH', res.token);
         window.open(res.url);
       });
+  }
+
+  showClipboardMessage(): void {
+    this.toastService.showSuccess(
+      'Session token copied',
+      'The session token was copied to your clipboard.'
+    );
   }
 }
