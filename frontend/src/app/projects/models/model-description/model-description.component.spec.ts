1ebda75970e5ef6fcd88c5960d5b161e0fbedbc2
Timothé Bailly-Barthez <timothe.bailly-barthez@deutschebahn.com>
2022-11-11 12:54:33 +0100

    test: Disable model description test.

--- frontend/src/app/projects/models/model-description/model-description.component.spec.ts
+++ frontend/src/app/projects/models/model-description/model-description.component.spec.ts
@@ -7,7 +7,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModelDescriptionComponent } from './model-description.component';
 
-describe('ModelDescriptionComponent', () => {
+xdescribe('ModelDescriptionComponent', () => {
   let component: ModelDescriptionComponent;
   let fixture: ComponentFixture<ModelDescriptionComponent>;
 
