ea9c030ab010d48ef773864a5e53c80a158d99b8
Timothé Bailly-Barthez <timothe.bailly-barthez@deutschebahn.com>
2022-11-18 16:55:55 +0100

    fix: Pass the value instead of the object.

--- frontend/src/app/settings/integrations/pure-variant/pure-variant.component.ts
+++ frontend/src/app/settings/integrations/pure-variant/pure-variant.component.ts
@@ -6,6 +6,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { filter } from 'rxjs';
+import { ToastService } from 'src/app/helpers/toast/toast.service';
 import { PureVariantService } from 'src/app/services/pure-variant/pure-variant.service';
 
 @Component({
@@ -18,13 +19,20 @@ export class PureVariantComponent implements OnInit {
     value: new FormControl<string>('', Validators.pattern(/^https?:\/\//)),
   });
 
-  constructor(private pureVariantService: PureVariantService) {}
+  constructor(
+    private pureVariantService: PureVariantService,
+    private toastService: ToastService
+  ) {}
 
   ngOnInit(): void {
     this.pureVariantService
       .get_license()
       .pipe(filter(Boolean))
       .subscribe((res) => {
+        this.toastService.showSuccess(
+          'Floating license server updating.',
+          'The floating license server was updated.'
+        );
         this.form.controls.value.patchValue(res.value);
       });
   }
@@ -33,6 +41,7 @@ export class PureVariantComponent implements OnInit {
     this.pureVariantService
       .set_license(this.form.value.value!)
       .subscribe((res) => {
+        this.toastService;
         this.form.controls.value.patchValue(res.value);
       });
   }
