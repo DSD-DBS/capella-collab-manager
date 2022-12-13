c62d5ab1eafa6b167f7bde6d81f6e90150ee95d3
dominik003 <info@dominik-lammers.de>
2022-12-13 11:45:36 +0100

    feat: Add events component
    This adds the component that allows administrators to see all
    events in one place. All columns, except the "Reason" column,
    can be sorted.

---
+++ frontend/src/app/events/events.component.ts
@@ -0,0 +1,74 @@
+/*
+ * SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { HistoryEvent } from '../services/user/user.service';
+import { EventsService } from './service/events.service';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css'],
+})
+export class EventsComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+  @ViewChild(MatSort) sort: MatSort | null = null;
+
+  displayedColumns: string[] = [
+    'eventType',
+    'userName',
+    'executorName',
+    'executionTime',
+    'projectName',
+    'reason',
+  ];
+
+  historyEventData: HistoryEvent[] = [];
+  historyEventDataSource = new MatTableDataSource<HistoryEvent>(
+    this.historyEventData
+  );
+
+  constructor(private eventService: EventsService) {}
+
+  ngOnInit(): void {
+    this.eventService.historyEvents.subscribe((historyEvents) => {
+      this.historyEventData = historyEvents;
+      this.historyEventDataSource.data = this.historyEventData;
+    });
+    this.eventService.loadHistoryEvents();
+  }
+
+  ngAfterViewInit(): void {
+    this.historyEventDataSource.paginator = this.paginator;
+    this.historyEventDataSource.sortingDataAccessor =
+      this.customSortingDataAccessor;
+    this.historyEventDataSource.sort = this.sort;
+  }
+
+  private customSortingDataAccessor(
+    data: HistoryEvent,
+    sortHeaderId: string
+  ): string | number {
+    switch (sortHeaderId) {
+      case 'eventType':
+        return data.event_type;
+      case 'userName':
+        return data.user.name;
+      case 'executorName':
+        return data.executor ? data.executor.name : 'System';
+      case 'executionTime':
+        return data.execution_time;
+      case 'projectName':
+        return data.project ? data.project.name : '';
+      case 'reason':
+        return data.reason;
+      default:
+        return '';
+    }
+  }
+}
