5267b3db5f0fcbc22f11eebff6c969e280dc75f0
Arjan Molenaar <gaphor@gmail.com>
2023-02-09 10:02:35 +0100

    fix: move jupyter startup to a script

---
+++ images/jupyter/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
+# SPDX-License-Identifier: Apache-2.0
+
+mkdir -p "$NOTEBOOKS_DIR"
+
+jupyter-lab --ip=0.0.0.0 \
+    --port=$JUPYTER_PORT \
+    --no-browser \
+    --ServerApp.base_url="$JUPYTER_BASE_URL" \
+    --ServerApp.root_dir="$NOTEBOOKS_DIR"
