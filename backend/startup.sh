e2acca90522cfaf5ab7a68b8e7c55997e4e623a6
MoritzWeber <kontakt@moritz-weber.net>
2023-06-22 15:35:54 +0200

    fix: Update loki URL and add cluster local to no_proxy

--- backend/startup.sh
+++ backend/startup.sh
@@ -5,5 +5,5 @@
 set -e
 
 # When running inside the cluster, the k8s service host should not use the proxy
-export no_proxy=$no_proxy,$KUBERNETES_SERVICE_HOST,$no_proxy_additional
+export no_proxy=$no_proxy,$KUBERNETES_SERVICE_HOST,$no_proxy_additional,svc.cluster.local
 uvicorn capellacollab.__main__:app --host 0.0.0.0
