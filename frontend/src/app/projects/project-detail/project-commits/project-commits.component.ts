583e356f8a549772ccd4a8b8c251af47da490ccb
romeonicholas <nicholas.romeo@code.berlin>
2023-11-30 10:03:24 +0100

    feat: Create project commits component and display in project details

---
+++ frontend/src/app/projects/project-detail/project-commits/project-commits.component.ts
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: Copyright DB Netz AG and the capella-collab-manager contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-commits',
+  templateUrl: './project-commits.component.html',
+})
+export class ProjectCommitsComponent {}
