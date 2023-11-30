064954188220a7703f94d86c339971bdc0bf11df
romeonicholas <nicholas.romeo@code.berlin>
2023-11-30 16:17:52 +0100

    feat: Add list of project models with dummy commit

--- frontend/src/app/projects/project-detail/project-commits/project-commits.component.ts
+++ frontend/src/app/projects/project-detail/project-commits/project-commits.component.ts
@@ -3,10 +3,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { untilDestroyed } from '@ngneat/until-destroy';
+import { filter } from 'rxjs';
+import {
+  Model,
+  ModelService,
+} from 'src/app/projects/models/service/model.service';
 
 @Component({
   selector: 'app-project-commits',
   templateUrl: './project-commits.component.html',
 })
-export class ProjectCommitsComponent {}
+export class ProjectCommitsComponent implements OnInit {
+  models?: Model[];
+
+  constructor(public modelService: ModelService) {}
+
+  ngOnInit(): void {
+    this.modelService.models$
+      .pipe(untilDestroyed(this), filter(Boolean))
+      .subscribe((models) => (this.models = models));
+  }
+}
