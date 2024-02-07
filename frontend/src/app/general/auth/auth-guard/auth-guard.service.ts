c0a1045e378002de33eb92a9c520cf90c5492fae
MoritzWeber <kontakt@moritz-weber.net>
2024-02-07 16:27:46 +0100

    ci: Reenable deprecation ESLint rule
    We had to disable the deprecation rule due to many deprecated modules
    after an Angular update. After replacing all deprecated modules step by step,
    we can reenable the rule again.

--- frontend/src/app/general/auth/auth-guard/auth-guard.service.ts
+++ frontend/src/app/general/auth/auth-guard/auth-guard.service.ts
@@ -3,37 +3,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
-  CanActivate,
+  CanActivateFn,
   RouterStateSnapshot,
-  UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuardService implements CanActivate {
-  constructor(private authService: AuthService) {}
+export const authGuard: CanActivateFn = (
+  _route: ActivatedRouteSnapshot,
+  _state: RouterStateSnapshot,
+) => {
+  const authService = inject(AuthService);
 
-  canActivate(
-    _route: ActivatedRouteSnapshot,
-    _state: RouterStateSnapshot,
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    if (this.authService.isLoggedIn()) {
-      return true;
-    } else {
-      // Needs window.location, since Router.url isn't updated yet
-      this.authService.cacheCurrentPath(window.location.pathname);
-      this.authService.webSSO();
-      return false;
-    }
+  if (authService.isLoggedIn()) {
+    return true;
+  } else {
+    // Needs window.location, since Router.url isn't updated yet
+    authService.cacheCurrentPath(window.location.pathname);
+    authService.webSSO();
+    return false;
   }
-}
+};
