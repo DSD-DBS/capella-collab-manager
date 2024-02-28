10cc69fd25efdf07d03fae5600355aec55681c49
MoritzWeber <kontakt@moritz-weber.net>
2024-02-28 16:08:49 +0100

    feat: Expose metrics about used TeamForCapella license usage
    Two new gauge metrics `used_t4c_licenses` and `total_t4c_licenses`
    were added to the `/metrics` endpoint,
    which contain the number of currently used and total licenses per TeamForCapella
    instance. The instance name is used as label for the metric.
    
    Note: If you have multiple TeamForCapella server instances, which point
    to the same license server, this is not detected. Therefore, make sure to
    filter the `t4c_licenses` for one instance first.

---
+++ backend/capellacollab/settings/modelsources/t4c/metrics.py
@@ -0,0 +1,66 @@
+# SPDX-FileCopyrightText: Copyright DB InfraGO AG and contributors
+# SPDX-License-Identifier: Apache-2.0
+
+import typing as t
+
+import prometheus_client
+import prometheus_client.core
+
+from capellacollab.core import database
+
+from . import crud, interface
+
+
+class UsedT4CLicensesCollector:
+    def collect(self) -> t.Iterable[prometheus_client.core.Metric]:
+        metric = prometheus_client.core.GaugeMetricFamily(
+            "used_t4c_licenses",
+            "Currently used T4C licenses per registered TeamForCapella instance.",
+            labels=["instance_name"],
+        )
+
+        with database.SessionLocal() as db:
+            instances = crud.get_t4c_instances(db)
+
+        if not instances:
+            return
+
+        for instance in instances:
+            try:
+                t4c_status = interface.get_t4c_status(instance)
+                used_licenses = t4c_status.total - t4c_status.free
+            except Exception:
+                used_licenses = -1
+
+            metric.add_metric([instance.name], used_licenses)
+        yield metric
+
+
+class TotalT4CLicensesCollector:
+    def collect(self) -> t.Iterable[prometheus_client.core.Metric]:
+        metric = prometheus_client.core.GaugeMetricFamily(
+            "total_t4c_licenses",
+            "Available licenses per registerd TeamForCapella instance.",
+            labels=["instance_name"],
+        )
+
+        with database.SessionLocal() as db:
+            instances = crud.get_t4c_instances(db)
+
+        if not instances:
+            return
+
+        for instance in instances:
+            try:
+                t4c_status = interface.get_t4c_status(instance)
+                total_licenses = t4c_status.total
+            except Exception:
+                total_licenses = -1
+
+            metric.add_metric([instance.name], total_licenses)
+        yield metric
+
+
+def register() -> None:
+    prometheus_client.REGISTRY.register(UsedT4CLicensesCollector())
+    prometheus_client.REGISTRY.register(TotalT4CLicensesCollector())
--- backend/tests/settings/conftest.py
+++ backend/tests/settings/conftest.py
@@ -4,8 +4,6 @@
 import pytest
 from sqlalchemy import orm
 
-from capellacollab.settings.modelsources.t4c import crud as t4c_crud
-from capellacollab.settings.modelsources.t4c import models as t4c_models
 from capellacollab.tools import crud as tools_crud
 from capellacollab.tools import models as tools_models
 from capellacollab.users import crud as users_crud
@@ -25,23 +23,3 @@ def fixture_admin_user(
     db: orm.Session, executor_name: str
 ) -> users_models.DatabaseUser:
     return users_crud.create_user(db, executor_name, users_models.Role.ADMIN)
-
-
-@pytest.fixture(name="t4c_instance")
-def fixture_t4c_instance(
-    db: orm.Session,
-    test_tool_version: tools_models.DatabaseVersion,
-) -> t4c_models.DatabaseT4CInstance:
-    server = t4c_models.DatabaseT4CInstance(
-        name="test server",
-        license="lic",
-        host="localhost",
-        usage_api="http://localhost:8086",
-        rest_api="http://localhost:8080/api/v1.0",
-        username="user",
-        password="pass",
-        protocol=t4c_models.Protocol.tcp,
-        version=test_tool_version,
-    )
-
-    return t4c_crud.create_t4c_instance(db, server)
