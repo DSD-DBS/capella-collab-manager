be63c6bab205eaab858c14e83ac69ead68e999ba
MoritzWeber <kontakt@moritz-weber.net>
2024-03-22 14:54:04 +0100

    feat: Skip loading of Jupyter file-shares if they don't exist
    If the file-shares don't exist as PVCs in the Kubernetes cluster,
    continue and start the session without the file-shares mounted.

--- backend/tests/projects/toolmodels/fixtures.py
+++ backend/tests/projects/toolmodels/fixtures.py
@@ -1,10 +1,12 @@
 # SPDX-FileCopyrightText: Copyright DB InfraGO AG and contributors
 # SPDX-License-Identifier: Apache-2.0
 
+import uuid
+
 import pytest
 from sqlalchemy import orm
 
-import capellacollab.projects.models as project_models
+import capellacollab.projects.models as projects_models
 import capellacollab.projects.toolmodels.crud as toolmodels_crud
 import capellacollab.projects.toolmodels.models as toolmodels_models
 import capellacollab.projects.toolmodels.modelsources.git.crud as project_git_crud
@@ -15,7 +17,7 @@ import capellacollab.tools.models as tools_models
 @pytest.fixture(name="capella_model")
 def fixture_capella_model(
     db: orm.Session,
-    project: project_models.DatabaseProject,
+    project: projects_models.DatabaseProject,
     capella_tool_version: tools_models.DatabaseVersion,
 ) -> toolmodels_models.DatabaseToolModel:
     model = toolmodels_models.PostCapellaModel(
@@ -23,6 +25,27 @@ def fixture_capella_model(
     )
     return toolmodels_crud.create_model(
         db, project, model, capella_tool_version.tool, capella_tool_version
+    )
+
+
+@pytest.fixture(name="jupyter_model")
+def fixture_jupyter_model(
+    db: orm.Session,
+    project: projects_models.DatabaseProject,
+    jupyter_tool: tools_models.DatabaseTool,
+) -> toolmodels_models.DatabaseToolModel:
+    jupyter_model = toolmodels_models.PostCapellaModel(
+        name="Jupyter test",
+        description="",
+        tool_id=jupyter_tool.id,
+        configuration={"workspace": str(uuid.uuid4())},
+    )
+    return toolmodels_crud.create_model(
+        db,
+        project,
+        jupyter_model,
+        tool=jupyter_tool,
+        configuration={"workspace": "test"},
     )
 
 
