d3f352bb3f13eee3edeff33fe75b8838c6812a6f
MoritzWeber <kontakt@moritz-weber.net>
2024-03-28 15:16:33 +0100

    feat: Add Storybook with Compodoc integration
    Storybook can be used to mock & test UI components.
    More information is available on their website:
    https://storybook.js.org/
    
    Resolves #1370

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: Copyright DB InfraGO AG and contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
