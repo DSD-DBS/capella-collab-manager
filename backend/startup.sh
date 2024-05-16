185db0562f82d947a836d78a2f56dd1d49f27d80
MoritzWeber <kontakt@moritz-weber.net>
2024-05-16 10:53:38 +0200

    fix: Trust proxy headers to avoid https -> http redirects
    When FastAPI sent a temporary redirect, it didn't respect the `X-Forwarded-Proto`
    header of the proxy.
    
    Therefore, it did default to `http` and sent an http URL as response.
    
    In this commit we tell Uvicorn to trust all IPs. Since the backend is firewalled
    and only reachable behind the Kubernetes Ingress / proxy, this is safe.

--- backend/startup.sh
+++ backend/startup.sh
@@ -7,4 +7,4 @@ set -e
 # When running inside the cluster, the k8s service host should not use the proxy
 export no_proxy=$no_proxy,$KUBERNETES_SERVICE_HOST,$no_proxy_additional,svc.cluster.local
 export NO_PROXY=$NO_PROXY,$KUBERNETES_SERVICE_HOST,$no_proxy_additional,svc.cluster.local
-uvicorn capellacollab.__main__:app --host 0.0.0.0
+uvicorn capellacollab.__main__:app --host 0.0.0.0 --forwarded-allow-ips '*'
