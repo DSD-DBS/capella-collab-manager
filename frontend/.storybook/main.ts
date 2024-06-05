86ee00745b9b9faac1054cee06f2948a5ac4732b
MoritzWeber <kontakt@moritz-weber.net>
2024-06-05 11:59:48 +0200

    fix: Reduce Storybook log level and fix warnings
    Storybook did spam the terminal with messages, which is especially
    annoying when running `make dev`.
    
    This PR reduces the log level by adding the --quite flag.
    
    In addition, there were some valid WARN messages about wrong usage
    of the files attribute in the `tsconfig.json`. Those warnings are resolved.

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -6,7 +6,7 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.ts'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
