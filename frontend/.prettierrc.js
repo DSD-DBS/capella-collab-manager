44afb556c98c739389743ca5ac99244a843dd113
MoritzWeber <kontakt@moritz-weber.net>
2024-11-13 12:23:34 +0100

    ci: Auto-format inline CSS classes with prettier

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -7,6 +7,8 @@ module.exports = {
   plugins: [
     require.resolve("prettier-plugin-tailwindcss"),
     require.resolve("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-classnames"),
+    require.resolve("prettier-plugin-merge"),
   ],
   importOrder: ["^[./]"],
   importOrderParserPlugins: ["typescript", "decorators-legacy"],
