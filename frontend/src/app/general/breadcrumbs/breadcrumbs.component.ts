068b7710abbe4dbe687b192fefbdd78bf09dc726
MoritzWeber <kontakt@moritz-weber.net>
2025-04-25 11:56:00 +0200

    feat: Fine-tune overview cards
    Overview cards like the project overview have a more modern
    hover color and the "Add new" cards have been optimized for
    mobile devices.

--- frontend/src/app/general/breadcrumbs/breadcrumbs.component.ts
+++ frontend/src/app/general/breadcrumbs/breadcrumbs.component.ts
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: Copyright DB InfraGO AG and contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-import { NgFor, AsyncPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BreadcrumbsService } from 'src/app/general/breadcrumbs/breadcrumbs.service';
@@ -10,8 +10,7 @@ import { BreadcrumbsService } from 'src/app/general/breadcrumbs/breadcrumbs.serv
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styleUrls: ['./breadcrumbs.component.css'],
-  imports: [NgFor, RouterLink, AsyncPipe],
+  imports: [RouterLink, AsyncPipe],
 })
 export class BreadcrumbsComponent {
   constructor(public readonly breadcrumbService: BreadcrumbsService) {}
