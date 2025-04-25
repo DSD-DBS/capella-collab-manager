30a72d076ffb295759bf0cc4fefac6e3b5fee3ec
MoritzWeber <kontakt@moritz-weber.net>
2025-04-25 14:55:00 +0200

    fix: Do show actions for own project user
    A project user shouldn't see the action to remove himself.

--- frontend/src/app/projects/project-detail/project-users/project-user-settings.component.ts
+++ frontend/src/app/projects/project-detail/project-users/project-user-settings.component.ts
@@ -70,9 +70,9 @@ export class ProjectUserSettingsComponent implements OnInit {
 
   constructor(
     public projectUserService: ProjectUserService,
-    public userService: OwnUserWrapperService,
     private toastService: ToastService,
     private projectService: ProjectWrapperService,
+    public ownUserWrapperService: OwnUserWrapperService,
     private dialog: MatDialog,
     private router: Router,
   ) {}
@@ -108,7 +108,7 @@ export class ProjectUserSettingsComponent implements OnInit {
     const dialogRef = this.dialog.open(InputDialogComponent, {
       data: {
         title: 'Remove User from Project',
-        text: `Do you really want to remove '${user.name}' from the project '${projectName}')? Please provide a reason.`,
+        text: `Do you really want to remove '${user.name}' from the project '${projectName}'? Please provide a reason.`,
       },
     });
 
